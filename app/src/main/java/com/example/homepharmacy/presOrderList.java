0e8fcc486148883347f1e0d6133caac5952df844
Koume95 <softregister95@gmail.com>
2020-08-16 22:32:20 +0530

    IT19127538

---
+++ app/src/main/java/com/example/homepharmacy/presOrderList.java
@@ -0,0 +1,14 @@
+package com.example.homepharmacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class presOrderList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pres_order_list);
+    }
+}
