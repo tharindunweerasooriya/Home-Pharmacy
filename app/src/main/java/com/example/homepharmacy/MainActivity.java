1e3304f9218bb43ae07a53fb144f86d2b9c86ae2
tharindunweerasooriya <68693917+tharindunweerasooriya@users.noreply.github.com>
2020-08-16 12:13:02 +0530

    first commit

---
+++ app/src/main/java/com/example/homepharmacy/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.homepharmacy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener( v -> startActivity(new Intent(MainActivity.this,ViewReport.class)));
+
+    }
+}
