4b1b89496238ea11f99833eb10f8f526f16113a5
tharindunweerasooriya <68693917+tharindunweerasooriya@users.noreply.github.com>
2020-08-16 19:59:08 +0530

    Ui Complete

--- app/src/main/java/com/example/homepharmacy/ReportHandling.java
+++ app/src/main/java/com/example/homepharmacy/ReportHandling.java
@@ -2,13 +2,19 @@ package com.example.homepharmacy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ReportHandling extends AppCompatActivity {
 
+    Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_handling);
+
+        button1 = findViewById(R.id.button4);
+        button1.setOnClickListener( v -> startActivity(new Intent(ReportHandling.this,ReportCard.class)));
     }
 }
